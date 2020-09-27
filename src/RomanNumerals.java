c868d03b4d67df2682fb7623cd49655cafa93322
Ande310g <64548557+ande310g@users.noreply.github.com>
2020-09-27 14:28:10 +0200

    opgave 1 done

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 public class RomanNumerals{
     public static void main(String[] args){
         int[] values = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
         String[] romanNumerals = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
+        //Scanner input = new Scanner(System.in);
+        int num = getInput();
         String output = "";
         int quantity;
         int remaining;
@@ -20,5 +21,22 @@ public class RomanNumerals{
         	}
         }
         System.out.println(output);
+    }
+    public static int getInput() {
+    	Scanner input = new Scanner(System.in);
+    	int num = 0;
+        try {
+        	num = input.nextInt();
+        	if (num > 0 && num <= 3000) {
+            	return num;
+        	} else {
+        		System.out.println("Wrong input, try again");
+            	return getInput();
+        	}
+        }
+        catch(Exception e) {
+        	System.out.println("Wrong input, try again");
+        	return getInput();
+        }
     }
 }
