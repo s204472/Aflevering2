13739fb2949b664d9981913f60f90ac2813827a1
Ande310g <64548557+ande310g@users.noreply.github.com>
2020-09-27 14:05:07 +0200

    First Program

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class RomanNumerals{
     public static void main(String[] args){
+        int[] values = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        String[] romanNumerals = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        String output = "";
+        int quantity;
+        int remaining;
         
+        for(int i = 12; i>=0; i--) {
+        	if (num/values[i]>0) {
+        		quantity = num/values[i];
+        		remaining = num%values[i];
+        		num = remaining;
+        		for(int j = 0; j < quantity; j++) {
+        			output += romanNumerals[i];
+        		}
+        	}
+        }
+        System.out.println(output);
     }
 }
--- src/opgave1.java
+++ src/opgave1.java
@@ -2,9 +2,7 @@
 public class opgave1 {
 
 	public static void main(String[] args) {
-		int num = 956;
-		int n = 900;
-		System.out.println(num/n);
-	}
+		
+		}
 
 }
