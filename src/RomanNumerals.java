96f30c94393dcfbb75d41bd282253c5e49800cb6
Ande310g <64548557+ande310g@users.noreply.github.com>
2020-09-27 13:35:48 +0200

    Prøver lige igen igen

---
+++ src/RomanNumerals.java
@@ -0,0 +1,5 @@
+public class RomanNumerals{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/opgave1.java
@@ -0,0 +1,10 @@
+
+public class opgave1 {
+
+	public static void main(String[] args) {
+		int num = 956;
+		int n = 900;
+		System.out.println(num/n);
+	}
+
+}
